eb1bfe2395584f38f259b31c8083fea65e9177ac
Joey Draper <joeyddraper@gmail.com>
2017-11-19 12:19:41 -0600

    moving files around to get static right

--- pymusic/settings.py
+++ pymusic/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 ]
+#'whitenoise.runserver_nostatic',
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
