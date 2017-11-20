97deb2bfbcf6c3665d230c7d1c95acfc39bcbb11
Joey Draper <joeyddraper@gmail.com>
2017-11-20 00:54:43 -0600

    changed urls

--- pymusic/urls.py
+++ pymusic/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include,url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('plist_concerts.urls')),
+    url(r'^', include('plist_concerts.urls')),
     url(r'^admin/', admin.site.urls),
 ]
